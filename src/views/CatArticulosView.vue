a2e4b17e95c81421346245727ed8a0276d3535cf
jmedina <javie@DEV-JM>
2024-10-25 16:05:11 -0700

    se agrega la primera pantalla de la pratica

---
+++ src/views/CatArticulosView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div> 
+    <h1>Catalogo de Articulos</h1>
+    <div>
+        <div class="d-flex justify-content-center flex-grow-0 flex-wrap">
+            
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            articulos: [
+                {
+                    codArticulo: 0,
+                    descripcion: 'Articulo de Prueba',
+                    precio:9999.99,
+                    urlImagen: ''
+                },
+                {
+                    codArticulo: 1,
+                    descripcion: 'Articulo de Prueba1',
+                    precio:9999.99,
+                    urlImagen: ''
+                },
+                {
+                    codArticulo: 2,
+                    descripcion: 'Articulo de Prueba2',
+                    precio:9999.99,
+                    urlImagen: ''
+                },
+                {
+                    codArticulo: 3,
+                    descripcion: 'Articulo de Prueba3',
+                    precio:9999.99,
+                    urlImagen: ''
+                }
+            ]
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-   
+    <input type="text" class="form-control" placeholder="Este Es Un Input de Bootstrap">
+    <button class="btn btn-primary">Hola</button>
   </div>
 </template>
 
 <script>
-
 
 export default {
   name: 'HomeView',
